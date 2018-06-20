344d52e22b110ec5502df25eb6521d570e6d7f72
Hanna <hanna.meyer1@gmail.com>
2018-06-20 10:24:21 +0200

    moved to own repository

---
+++ src/04_mergedatasets.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+setwd("/media/hanna/data/IDESSA_TAIR/extracted/")
+
+dats <- list.files(,pattern="StationMatch")
+
+dataset <- data.frame()
+for (i in 1:length(dats)){
+  dat <- get(load(dats[i]))
+  dataset <- rbind(dataset,dat)
+}
+
+names(dataset)[names(dataset)=="rainfall"]<-"Tair"
+save(dataset,file="Tair_dataset.RData")
