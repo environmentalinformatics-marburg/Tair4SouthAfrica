a43a536d3bd78a8d3e239b50cd58c0b02d6194d5
HannaMeyer <hanna.meyer@geo.uni-marburg.de>
2018-09-14 15:49:30 +0200

    clean up

--- src/00_testNDVICor.R
+++ src/00_testNDVICor.R
@@ -1,6 +1,11 @@
 rm(list=ls())
 library(raster)
 library(rgdal)
+library(RColorBrewer)
+mainpath <- "/home/hanna/Documents/Projects/IDESSA/airT/forPaper/"
+modelpath <- paste0(mainpath,"/modeldat")
+vispath <- paste0(mainpath,"visualizations/")
+
 load("/home/hanna/Documents/Projects/IDESSA/airT/forPaper/modeldat/modeldata.RData")
 shp <- readOGR("/home/hanna/Documents/Projects/IDESSA/airT/forPaper/shp/WeatherStations.shp")
 
@@ -16,4 +21,18 @@ stationCor <- data.frame("Station"=unique(dataset$Station),"cor"=stationCor)
 shp <- shp[shp$plot%in%stationCor$Station,]
 shp$ndvicor <- merge(shp@data,stationCor,by.y="Station",by.x="plot")$cor
 
-spplot(shp,"ndvicor")
+####plot
+base <- readOGR("/home/hanna/Documents/Projects/IDESSA/GIS/TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp",
+                "TM_WORLD_BORDERS-0.3")
+#shp$ndvicor<-round(shp$ndvicor,2)
+
+pdf(paste0(vispath,"/ndvicorr.pdf"),width=5.5,height=5.5)
+spplot(shp,"ndvicor",pretty=TRUE,
+       key.space=list(x = 0.68, y = 0.18, cex=0.7,corner = c(0, 1)),
+       col.regions=rev(brewer.pal(5, "RdBu")), 
+       cuts=5,scales=list(draw = TRUE),
+       sp.layout=list(
+  "sp.polygons", base, col = "black", fill="grey90",first = TRUE)
+)
+dev.off()
+
--- src/09_spatialPrediction.R
+++ src/09_spatialPrediction.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(raster)
 library(rgdal)
 library(caret)
-mainpath <-"/media/memory02/users/hmeyer/IDESSA_TAIR/"
+mainpath <-"/mnt/sd19007/users/hmeyer/IDESSA_TAIR/"
 auxpath <- paste0(mainpath,"auxiliary/")
 MSGpath <-  paste0(mainpath,"MSG/")
 modelpath <- paste0(mainpath,"modeldat/")
@@ -21,14 +21,15 @@ model <- get(load(paste0(modelpath,"model_final.RData")))
 for (i in 1:length(MSGscenes)){
   MSGscene <- stack(MSGscenes[i],predictors)
   names(MSGscene) <- c("VIS0.6", "VIS0.8", 
-                        "NIR1.6", "IR3.9", "WV6.2", "WV7.3", "IR8.7", "IR9.7", "IR10.8", 
-                        "IR12.0", "IR13.4","sunzenith",
-                        "Continentality","Biome","Prec",
-                        "Tmean","Dem","Biome_agg","Precseason")
+                       "NIR1.6", "IR3.9", "WV6.2", "WV7.3", 
+                       "IR8.7", "IR9.7", "IR10.8", 
+                       "IR12.0", "IR13.4","sunzenith",
+                       "Continentality","Biome","Prec",
+                       "Tmean","Dem","Biome_agg","Precseason")
   prediction <- predict(MSGscene,model)
-  writeRaster(prediction,
+  writeRaster(prediction*10,
               paste0(outpath,"prediction_",substr(MSGscenes[i],nchar(MSGscenes[i])-13,nchar(MSGscenes[i]))),
-              overwrite=TRUE)
+              overwrite=TRUE,datatype='INT2S')
   
 }
 
